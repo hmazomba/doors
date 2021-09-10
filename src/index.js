d5cb6ffcb8c3b451b8b48623a361694f67d00dac
Hlumisa Mazomba <hmazomba@live.co.za>
2021-09-10 12:40:38 +0200

    Added a search component

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Single from './components/Single' 
+import Search from './components/Search';
 
 const routing = (
 	<Router>
@@ -21,6 +22,7 @@ const routing = (
 				<Route path="/login" component={Login} />
 				<Route path="/logout" component={Logout} />
 				<Route path="/post/:slug" component={Single} />
+				<Route path="/search" component={Search} />
 			</Switch>
 			<Footer />
 		</React.StrictMode>
